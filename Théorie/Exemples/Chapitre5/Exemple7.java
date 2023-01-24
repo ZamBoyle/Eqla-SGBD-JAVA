133a3efd3f66cc537756cf938d5b9089081dd157
Johnny Piette <johnny.piette@gmail.com>
2023-01-24 01:01:46 +0100

    Updates

--- Théorie/Exemples/Chapitre5/Exemple7.java
+++ Théorie/Exemples/Chapitre5/Exemple7.java
@@ -9,7 +9,7 @@ public class Exemple7 {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         addLecteur();
-    }
+    } 
 
     public static void addLecteur(){
         scanner = new Scanner(System.in);
